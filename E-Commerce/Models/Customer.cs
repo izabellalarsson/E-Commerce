770f87da93b05687ff550261373508c06a6c1fe0
Izabella <izzwii.izabella@gmail.com>
2019-03-22 12:22:56 +0100

    started building orders

---
+++ E-Commerce/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ECommerce.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Zipcode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public Customer()
+
+        {
+        }
+    }
+}
---
+++ E-Commerce/Models/Orders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ECommerce.Models
+{
+    public class Orders
+    {
+        public int OrderId { get; set; }
+        public int CostumerId { get; set; }
+        public int TotalPrice { get; set; }
+        public Cart Cart { get; set; }
+        
+         public Orders()
+        {
+        }
+    }
+}
